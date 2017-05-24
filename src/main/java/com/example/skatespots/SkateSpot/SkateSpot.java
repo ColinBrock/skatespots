001842e5fa17657182a82c15fc5c1598858cff19
Chris Reams <chrisreams91@gmail.com>
2017-05-24 14:35:35 -0500

    first commit z

---
+++ src/main/java/com/example/skatespots/SkateSpot/SkateSpot.java
@@ -0,0 +1,25 @@
+package com.example.skatespots.SkateSpot;
+
+import java.util.Date;
+
+/**
+ * Created by chris on 5/24/17.
+ */
+public class SkateSpot {
+
+    private String name;
+    private String description;
+    private Date date;
+    private String owner;
+    
+
+
+    private String getName() {
+        return name;
+    }
+
+    private String getDescription() {
+        return description;
+    }
+
+}
